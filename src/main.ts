c9144b41e72af93e8bbc4041268892f7bafeba5e
Christian Toledo <chrstiantoledo@gmail.com>
2020-12-18 14:33:29 -0300

    updatcenter point

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
+//import vuetify from "./plugins/vuetify";
 
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
 
